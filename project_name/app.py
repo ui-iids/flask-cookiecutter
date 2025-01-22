f764fd9c249b551107734934c93992cc7d1c2704
Clinton Bradford <clintonbradford@gmail.com>
2025-01-22 09:10:31 -0800

    Improve app.py documentation

--- project_name/app.py
+++ project_name/app.py
@@ -5,13 +5,24 @@ from collections import namedtuple
 
 from .pages import pages
 from .api import register_apis
+from .database import register_database
 
 
 def create_app(config_filename="config.toml", config_override={}):
     app = Flask(__name__)
 
+    # Register Database
+    # * Remove this and `models.py`, `database.py` if database is unneeded.
+    # * Add `psycopg-binary` if PostGres is needed.
+    # * Add `geoalchemy2` if Geographic/Geometric queries are needed.
+    register_database(app)
+
+    # Register Rendered Pages Blueprint
+    # * Remove if only need an API.
     app.register_blueprint(pages)
 
+    # Error Handlers
+    # Customize to the richness/communication/sensitivity needed for the project at hand.
     @app.errorhandler(exceptions.NotFound)
     def page_not_found(e):
         return render_template("pages/404.html"), 404
@@ -20,11 +31,19 @@ def create_app(config_filename="config.toml", config_override={}):
     def server_error(e):
         return render_template("pages/error.html"), 500
 
+    # Accepts config from:
+    # * A file at `project_name/config.toml` or a name specified in `config_filename`
+    # * A Python dictionary or dictionary-like passed as `config_override`
+    # * argument, or a file specified by environment variable `APP_CONFIG_FILE`
+    #
+    # Some relevant configuration is set downstream in the individual blueprints or modules,
+    # Such as Database Connection URIs or API metadata.
     app.config.from_file(config_filename, load=tomlload, text=False, silent=True)
     app.config.from_object(namedtuple("Config", config_override)(**config_override))
     app.config.from_envvar("APP_CONFIG_FILE", silent=True)
 
-    # For debug purposes, we compile CSS every time it changes
+    # For debug purposes, we compile CSS every time it changes.
+    # For deployments, ensure the `static.css` is up to date.
     if app.debug:
         from sassutils.wsgi import SassMiddleware
 
@@ -40,8 +59,12 @@ def create_app(config_filename="config.toml", config_override={}):
             },
         )
 
+    # Register the APIs paths from `project_name/api`.
+    # Remove if API is not needed.
     register_apis(app)
 
+    # Serve a basic PWA application with a static `offline` page.
+    # Extend the service worker for more advanced cached functionality.
     if app.config.get("SERVE_PWA"):
 
         @app.route("/manifest.json", methods=["GET"])
