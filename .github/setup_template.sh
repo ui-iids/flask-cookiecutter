21c0cdc1bf952a9baa37321c5d380e3e944bf6ea
Clinton Bradford <clintonbradford@gmail.com>
2025-07-09 12:04:27 -0700

    Rework templating logic to account for dashed variants of names

--- .github/setup_template.sh
+++ .github/setup_template.sh
@@ -18,19 +18,25 @@ echo "Renaming project..."
 
 original_author="author_name"
 original_name="project_name"
+original_dash_name="project-name"
 original_urlname="project_urlname"
 original_description="project_description"
+
+# Convert underscores to dashes, and upper to lowercase
+dash_name=$(echo $name | tr '[:upper:]' '[:lower:]' | tr '_' '-')
+underscore_name=$(echo $name | tr '[:lower:]' '[:upper:]' | tr '-' '_')
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
     sed -i "s/$original_author/$author/g" $filename
-    sed -i "s/$original_name/$name/g" $filename
+    sed -i "s/$original_name/$underscore_name/g" $filename
+    sed -i "s/$original_dash_name/$dash_name/g" $filename
     sed -i "s/$original_urlname/$urlname/g" $filename
     sed -i "s/$original_description/$description/g" $filename
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv project_name $underscore_name
 mv -f project_templates/* .
 
 # This command runs only once on GHA!
